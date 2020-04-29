426f1259d86bda4e1581345c1ed762b66ae16c5e
Dmitry Fedorinov <skinklass@mail.ru>
2020-04-29 11:05:23 +0300

    Initial Commit

---
+++ CheckWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckWeather
+//
+//  Created by Дмитрий Федоринов on 29.04.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
